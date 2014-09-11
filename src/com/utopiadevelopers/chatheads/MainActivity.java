c004cfb4ad4fcb317db49d0472f03f0250fd2c6c
Satyam Krishna <satyamkrishna92@gmail.com>
2014-09-11 22:16:46 +0530

    Service Setup

--- src/com/utopiadevelopers/chatheads/MainActivity.java
+++ src/com/utopiadevelopers/chatheads/MainActivity.java
@@ -1,36 +1,58 @@
 package com.utopiadevelopers.chatheads;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+public class MainActivity extends ActionBarActivity implements OnClickListener
+{
+	public Button start;
+	public Button stop;
 
-public class MainActivity extends ActionBarActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		start = (Button) findViewById(R.id.btnStart);
+		stop = (Button) findViewById(R.id.btnStop);
+		start.setOnClickListener(this);
+		stop.setOnClickListener(this);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.btnStart:
+			start.setEnabled(false);
+			stop.setEnabled(true);
+			break;
+		case R.id.btnStop:
+			start.setEnabled(true);
+			stop.setEnabled(false);
+			break;
+		default:
+			break;
+		}
+	}
 }
---
+++ src/com/utopiadevelopers/service/ChatHeadService.java
@@ -0,0 +1,15 @@
+package com.utopiadevelopers.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ChatHeadService extends Service
+{
+	@Override
+	public IBinder onBind(Intent intent) 
+	{	
+		return null;
+	}
+
+}
