78d8eb4605665b07b1ef3551891ed1b39c7214a0
Satyam Krishna <satyamkrishna92@gmail.com>
2014-09-19 09:05:53 +0530

    Chat Head Using Spring

---
+++ src/com/utopiadevelopers/service/ChatHead.java
@@ -0,0 +1,42 @@
+package com.utopiadevelopers.service;
+
+import android.content.Context;
+import android.view.View;
+
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringListener;
+
+public class ChatHead extends View implements SpringListener
+{
+
+	public ChatHead(Context context)
+	{
+		super(context);
+
+	}
+
+	@Override
+	public void onSpringActivate(Spring arg0)
+	{
+
+	}
+
+	@Override
+	public void onSpringAtRest(Spring arg0)
+	{
+
+	}
+
+	@Override
+	public void onSpringEndStateChange(Spring arg0)
+	{
+
+	}
+
+	@Override
+	public void onSpringUpdate(Spring arg0)
+	{
+
+	}
+
+}
